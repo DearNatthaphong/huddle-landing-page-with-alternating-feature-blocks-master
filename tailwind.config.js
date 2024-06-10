82f5fb9cedac2f47feeb2b4da8e5879ca4e4bde4
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-06-11 04:35:39 +0700

    feat: add responsive layout with hover states using Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primaryPink: 'hsl(322, 100%, 66%)',
+        neutralVeryPaleCyan: 'hsl(193, 100%, 96%)',
+        neutralVeryDarkCyan: 'hsl(192, 100%, 9%)',
+        neutralGrayishBlue: 'hsl(208, 11%, 55%)'
+      },
+      fontFamily: {
+        poppins: ['Poppins'],
+        openSans: ['Open Sans']
+      }
+    }
+  }
+};
